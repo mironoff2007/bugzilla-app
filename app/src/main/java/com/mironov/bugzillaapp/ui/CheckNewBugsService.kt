56bbe67fa3a1de89901eb1bc4e69ba9aee4ca9dc
Mironov Ury <mironoff2007@rambler.ru>
2022-02-20 13:47:12 +0300

    остановить сервис по нажатию на текст

--- app/src/main/java/com/mironov/bugzillaapp/ui/CheckNewBugsService.kt
+++ app/src/main/java/com/mironov/bugzillaapp/ui/CheckNewBugsService.kt
@@ -1,18 +1,19 @@
 package com.mironov.bugzillaapp.ui
 
-import android.R
 import android.app.*
+import android.app.PendingIntent.getService
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
+import com.mironov.bugzillaapp.R
 import com.mironov.bugzillaapp.appComponent
 import com.mironov.bugzillaapp.data.BaseRepository
-import com.mironov.bugzillaapp.data.Repository
 import com.mironov.bugzillaapp.data.retrofit.ApiResponse
 import com.mironov.bugzillaapp.domain.DateUtil
 import kotlinx.coroutines.CoroutineScope
@@ -55,11 +56,19 @@ class CheckNewBugsService : Service() {
                 // https://developer.android.com/reference/android/support/v4/app/NotificationCompat.Builder.html#NotificationCompat.Builder(android.content.Context)
                 ""
             }
-    }
-
-    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
         id++
         serviceId = id
+
+    }
+
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+
+        Log.d("My_tag", intent.action.toString())
+
+        if (ACTION_STOP_SERVICE == intent.action) {
+            stopService()
+        }
 
         val notification =
             buildNotification(applicationContext.getString(com.mironov.bugzillaapp.R.string.no_new_bugs))
@@ -68,8 +77,6 @@ class CheckNewBugsService : Service() {
         serviceScope.launch(Dispatchers.Default) {
             fixedRateTimer(TIMER_NAME, false, INITIAL_DELAY, TIMER_PERIOD)
             {
-
-                updateNotification()
                 repository.getBugsFromNetworkByDate(DateUtil.getTodayDate()).enqueue(object :
                     Callback<ApiResponse?> {
                     override fun onResponse(
@@ -95,16 +102,26 @@ class CheckNewBugsService : Service() {
     }
 
     private fun buildNotification(contextText: String): Notification {
+        val stopSelf = Intent(ACTION_STOP_SERVICE)
+        stopSelf.setClass(this@CheckNewBugsService, CheckNewBugsService::class.java)
+        stopSelf.action = ACTION_STOP_SERVICE
+
+        val pStopSelf = getService(this, 0, stopSelf, PendingIntent.FLAG_CANCEL_CURRENT)
+
         return NotificationCompat.Builder(this, channelId)
-            .setContentTitle(applicationContext.getString(com.mironov.bugzillaapp.R.string.bug_service_name))
+            .setContentTitle(applicationContext.getString(R.string.bug_service_name))
             .setContentText(contextText)
-            .setSmallIcon(R.drawable.ic_popup_sync)
+            .setSmallIcon(R.drawable.ic_bug)
+            .addAction(android.R.drawable.ic_menu_close_clear_cancel,
+                applicationContext.getString(R.string.close),
+                pStopSelf
+            )
             .build()
     }
 
     private fun updateNotification() {
         val notification =
-            buildNotification(applicationContext.getString(com.mironov.bugzillaapp.R.string.new_bugs))
+            buildNotification(applicationContext.getString(R.string.new_bugs))
         val mNotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         mNotificationManager.notify(serviceId, notification)
     }
@@ -123,6 +140,7 @@ class CheckNewBugsService : Service() {
     }
 
     fun stopService() {
+        stopForeground(true)
         stopSelf()
     }
 
@@ -145,6 +163,7 @@ class CheckNewBugsService : Service() {
         const val TIMER_PERIOD = 1000L
         const val INITIAL_DELAY = 1000L
         const val TIMER_NAME = "TIMER_NAME"
+        const val ACTION_STOP_SERVICE = "STOP"
         const val BUGS_SERVICE_CHANNEL_ID = "BUGS_SERVICE_CHANNEL_ID"
         const val BUGS_SERVICE_CHANNEL_NAME = "BUGS_SERVICE_CHANNEL_NAME"
     }
