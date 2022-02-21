c837aaebfc9d44028bf87b311cb0814cd7519530
Mironov Ury <mironoff2007@rambler.ru>
2022-02-21 11:20:39 +0300

    readme обновил

--- app/src/main/java/com/mironov/bugzillaapp/data/retrofit/NetworkService.kt
+++ app/src/main/java/com/mironov/bugzillaapp/data/retrofit/NetworkService.kt
@@ -5,14 +5,10 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-
 object NetworkService {
 
-    //private var mInstance: NetworkService? = null
-    //https://bugzilla.mozilla.org/rest/bug?creation_time=2022-02-15
     private val BASE_URL = "https://bugzilla.mozilla.org/rest/"
-    private val apiKey = "Qry2P0jEMVbJxnxuBz9JJrk5A6keESQYYYs63c5B"
-    private lateinit var mRetrofit: Retrofit
+    private var mRetrofit: Retrofit
 
     init {
         val client = OkHttpClient.Builder()
